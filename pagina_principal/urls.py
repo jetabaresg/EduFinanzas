109282524b4027044365aa03fbc9a2b5fcf747c9
loterini0 <jloteror@unal.edu.co>
2025-11-14 23:49:22 -0500

    Add: vistas de iniciar sesión y registrarse, actualización de vistas existentes

--- pagina_principal/urls.py
+++ pagina_principal/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('acerca/', views.acerca, name='acerca'),
     path('aprende/', views.aprende, name='aprende'),
     path('aprende/<str:banco_id>/', views.banco_detalle, name='banco_detalle'),
-    path('feedback/', views.feedback, name='feedback'),  
-    path('gracias-feedback/', views.gracias_feedback, name='gracias_feedback'), 
+    path('feedback/', views.feedback, name='feedback'),
+    
 ]
+    
